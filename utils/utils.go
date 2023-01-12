b8a7497000e84ad9ad780e4005979bd860ea1985
Suptree <daiki.minobe@gmail.com>
2023-01-13 05:43:43 +0900

    acceess allow origin

--- utils/utils.go
+++ utils/utils.go
@@ -12,12 +12,20 @@ import (
 
 // RespondWithError エラー情報をJSONで返す
 func RespondWithError(w http.ResponseWriter, code int, msg string) {
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
 	RespondWithJSON(w, code, map[string]string{"error": msg})
 }
 
 // RespondWithJSON JSONを返す部分を共通化
 func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
